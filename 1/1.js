b07bc7cea4facd11f16b50d752ce207b0fa4ef4d
Zalonics <anthony.dev.9898@gmail.com>
2022-12-01 11:02:19 -0800

    Day 1 Refactor

--- 1/1.js
+++ 1/1.js
@@ -2,12 +2,8 @@ const fs = require('fs')
 
 const input = fs.readFileSync('./input.txt', 'utf-8').split('\n\n')
 let elves = []
+
 for (const calories of input) {
-    elves.push(
-        calories
-            .split('\n')
-            .map((num) => +num)
-            .reduce((acc, sum) => acc + sum, 0)
-    )
+    elves.push(calories.split('\n').reduce((sum, num) => +num + sum, 0))
 }
 console.log(Math.max(...elves))
