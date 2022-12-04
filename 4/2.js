f110869398f1b1c5baa8a69d2300807d146616d6
Zalonics <anthony.dev.9898@gmail.com>
2022-12-04 06:51:40 -0800

    Day 4 Initial Complete

---
+++ 4/2.js
@@ -0,0 +1,38 @@
+const { readFileSync } = require('fs')
+
+const input = readFileSync('./input.txt', 'utf-8').split('\n')
+const solve = () => {
+    let count = 0
+    input.forEach((tasks) => {
+        const visited = {}
+        const [elf1_tasks, elf2_tasks] = tasks.split(',')
+        const [elf1_low, elf1_high] = elf1_tasks.split('-').map((num) => +num)
+        const [elf2_low, elf2_high] = elf2_tasks.split('-').map((num) => +num)
+        for (let i = elf1_low; i <= elf1_high; i++) {
+            if (!visited[i]) {
+                visited[i] = 1
+                continue
+            }
+            visited[i]++
+        }
+
+        for (let i = elf2_low; i <= elf2_high; i++) {
+            if (!visited[i]) {
+                visited[i] = 1
+                continue
+            }
+            visited[i]++
+        }
+        for (const key in visited) {
+            if (visited[key] > 1) {
+                count++
+                return
+            }
+        }
+        console.log(visited)
+    })
+
+    console.log(count)
+}
+
+solve()
