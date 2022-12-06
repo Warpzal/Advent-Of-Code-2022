a2357faae9b40906c605b7009f9a6f39b7c5f1a1
Zalonics <anthony.dev.9898@gmail.com>
2022-12-06 15:18:34 -0800

    Cleanup and clarity

--- 6/1.ts
+++ 6/1.ts
@@ -1,9 +1,9 @@
 import { readFileSync } from 'fs'
-const datastreamBuffer: string[] = readFileSync('./input.txt', 'utf-8').split(
-    ''
-)
+const datastreamBuffer: string = readFileSync('./input.txt', 'utf-8')
 
-const getFirstMarker = (buffer: string[]): number => {
+const getFirstMarker = (buffer: string): number => {
+    // Part 1: 4
+    // Part 2: 14
     const uniqueChars = 14
     for (let i = uniqueChars; i < buffer.length - uniqueChars + 1; i++) {
         const start = i - uniqueChars
